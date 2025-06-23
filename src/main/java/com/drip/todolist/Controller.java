576ffe3e8c1445afbe06abdab7f91d3020647d06
Ifeanyi <maduabuchimarvelous@gmail.com>
2025-06-23 14:12:03 +0100

    Implementing Key Events and Toolbars

--- src/main/java/com/drip/todolist/Controller.java
+++ src/main/java/com/drip/todolist/Controller.java
@@ -10,6 +10,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -130,6 +132,16 @@ public class Controller {
 			TodoItem newItem = controller.processResults();
 			todoListView.getSelectionModel().select(newItem);
 			System.out.println("OK pressed");
+		}
+	}
+
+	@FXML
+	public void handleKeyPressed(KeyEvent keyEvent) {
+		TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+		if(selectedItem != null) {
+			if(keyEvent.getCode().equals(KeyCode.DELETE)) {
+				deleteItem(selectedItem);
+			}
 		}
 	}
 
